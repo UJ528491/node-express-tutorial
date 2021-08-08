d2e585ab9c8aa2f84ea7d79b0cce2c15ce330932
UJ528491 <hyj0829@gmail.com>
2021-08-08 20:19:06 +0900

    node modules

---
+++ 3-modules.ts
@@ -0,0 +1,6 @@
+const names = require("./4-names");
+const sayHiFunc = require("./3-utils");
+
+sayHiFunc("susan");
+sayHiFunc(names.john);
+sayHiFunc(names.peter);
---
+++ 5-utils.ts
@@ -0,0 +1,5 @@
+const sayHi = (name: string) => {
+  console.log(`Hello ${name}`);
+};
+
+module.exports = sayHi;
