a5def0496948d497ec6deadb19e7aa90a0ac2e90
UJ528491 <hyj0829@gmail.com>
2021-08-15 13:41:05 +0900

    HTTP reqest url, response html

--- 02-express-tutorial/app.ts
+++ 02-express-tutorial/app.ts
@@ -1,1 +1,30 @@
-console.log('Express Tutorial')
+// const http = require('http')
+import http from "http";
+import { readFileSync } from "fs";
+
+// get all files
+const homePage = readFileSync("./index.html");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    // res.write("<h1>hi ujong</h1>");
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Page not found!</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
