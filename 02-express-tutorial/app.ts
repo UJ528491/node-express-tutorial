972fc8cac9df8813aae342d40e9c709e12b809b5
UJ528491 <hyj0829@gmail.com>
2021-08-17 20:37:28 +0900

    express basic

--- 02-express-tutorial/app.ts
+++ 02-express-tutorial/app.ts
@@ -1,50 +1,40 @@
-// const http = require('http')
-import http from "http";
-import { readFileSync } from "fs";
+// const express = require("express");
+import express from "express";
+const app = express();
 
-// get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyle = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  // home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  }
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page</h1>");
-    res.end();
-  }
-  // styles
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyle);
-    res.end();
-  }
-  // image/logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  }
-  // js
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  }
-  // 404
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Page not found!</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("HomePage");
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+// app.get("/", (req, res) => {
+//   console.log("user hit the resource");
+//   res.status(200).send("Home Page");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.status(200).send("About Page");
+// });
+
+// app.all("*", (req, res) => {
+//   res.status(404).send("<h1>resource not found</h1>");
+// });
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
