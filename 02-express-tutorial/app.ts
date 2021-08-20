a4120cf2b03ae7e3d2cab5e95b9a19bcd937caaa
UJ528491 <hyj0829@gmail.com>
2021-08-20 15:35:39 +0900

    router params

--- 02-express-tutorial/app.ts
+++ 02-express-tutorial/app.ts
@@ -3,19 +3,36 @@ const { products, people } = require("./data");
 
 const app = Express();
 
+interface products {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  desc: string;
+}
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
   // res.json(products);
 });
 
 app.get("/api/products", (req, res) => {
-  const newProducts: [] = products.map(
-    (product: { id: string; name: string; image: string }) => {
-      const { id, name, image } = product;
-      return { id, name, image };
-    }
-  );
+  const newProducts: [] = products.map((product: products) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
   res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct: [] = products.find((product: products) => {
+    return product?.id === Number(productID);
+  });
+  if (!singleProduct) {
+    return res.status(404).send("Product Does not Exist");
+  }
+  return res.json(singleProduct);
 });
 
 app.listen(5000, () => {
