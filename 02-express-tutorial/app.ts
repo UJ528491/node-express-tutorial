c672714e02c6f03893f72c6cf7d258972ea524bb
UJ528491 <hyj0829@gmail.com>
2021-08-26 15:23:17 +0900

    POST in javascript

--- 02-express-tutorial/app.ts
+++ 02-express-tutorial/app.ts
@@ -7,8 +7,21 @@ app.use(express.static("./methods-public"));
 // parse form data
 // urlencoded() : built-in middleware in express
 app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name vlaue" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
