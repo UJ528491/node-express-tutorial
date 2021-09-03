4167d6799693992784d815da338df773623ea233
UJ528491 <hyj0829@gmail.com>
2021-09-03 23:37:24 +0900

    basic express server

--- 03-task-manager/starter/app.ts
+++ 03-task-manager/starter/app.ts
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+import express from "express";
+const app = express();
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+const port = 3000;
+
+app.listen(port, () =>
+  console.log(`server is listening on port http://localhost:${port}`)
+);
