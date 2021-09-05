75268e7dc852c8b932663c07fb117a8c87622fc9
UJ528491 <hyj0829@gmail.com>
2021-09-05 23:40:47 +0900

    route structure

--- 03-task-manager/starter/app.ts
+++ 03-task-manager/starter/app.ts
@@ -6,6 +6,12 @@ app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
 
+// app.get('/api/v1/tasks')       - get all the tasks
+// app.post('/api/v1/tasks')     - create new task
+// app.get('/api/v1/tasks/:id')  - get single tasks
+// app.patch('/api/v1/tasks/:id'')   -update tasks
+// app.delete('/api/v1/tasks/:id'')   - delete tasks
+
 const port = 3000;
 
 app.listen(port, () =>
