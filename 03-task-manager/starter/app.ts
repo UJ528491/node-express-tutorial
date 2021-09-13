a64e51417ce2c839579a931c00c337360407f8f6
UJ528491 <hyj0829@gmail.com>
2021-09-13 23:58:36 +0900

    add dotenv

--- 03-task-manager/starter/app.ts
+++ 03-task-manager/starter/app.ts
@@ -1,7 +1,8 @@
-require("./db/connect");
 import express from "express";
 import { router } from "./routes/tasks";
-
+import { connectDB } from "./db/connect";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 // routes
@@ -18,7 +19,13 @@ app.use("/api/v1/tasks", router);
 // app.delete('/api/v1/tasks/:id'')   - delete tasks
 
 const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`server is listening on "http://localhost:${port}"`)
+    );
+  } catch (error) {}
+};
 
-app.listen(port, () =>
-  console.log(`server is listening on port http://localhost:${port}`)
-);
+start();
--- 03-task-manager/starter/db/connect.ts
+++ 03-task-manager/starter/db/connect.ts
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
-const pw = "dbwhd";
-const connectionString = `mongodb+srv://ujong:${pw}@nodeexpressprojects.7tglc.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority`;
-
-mongoose
-  .connect(connectionString, {
+export const connectDB = url => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("connect to the DB..."))
-  .catch(err => console.log(err));
+  });
+};
