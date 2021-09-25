1e3ab9dc25526c471595c4943de91bc48c374c5d
UJ528491 <hyj0829@gmail.com>
2021-09-26 00:00:46 +0900

    dotenv

--- 04-store-api/starter/app.ts
+++ 04-store-api/starter/app.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 import notFoundMiddleware from "../starter/middleware/not-found"
 import errorHandlerMiddleware from "../starter/middleware/error-handler"
+import connectDB from "./db/connect"
 const app = express()
 
 // midleware
@@ -22,6 +23,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
   try {
     // connectDB
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening "http://localhost:${port}"`)
     )
