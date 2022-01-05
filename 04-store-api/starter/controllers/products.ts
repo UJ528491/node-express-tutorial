dfe028b2fa2a9a96745fe4e6253b320d499ef87c
UJ528491 <hyj0829@gmail.com>
2022-01-05 22:29:42 +0900

    query object

--- 04-store-api/starter/controllers/products.ts
+++ 04-store-api/starter/controllers/products.ts
@@ -1,9 +1,18 @@
+import Product from "../models/product";
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing acync errors");
-  res.status(200).json({ msg: "products testing route" });
+  // throw new Error("testing acync errors");
+  const products = await Product.find({ page: "2" });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured } = req.qeury;
+  const qeuryObject = { featured };
+  if (featured) {
+    qeuryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 export { getAllProducts, getAllProductsStatic };
