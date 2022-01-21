11c1397e5babf9e303803c1f41b1b87981fff307
UJ528491 <hyj0829@gmail.com>
2022-01-22 01:32:39 +0900

    add sort

--- 04-store-api/starter/controllers/products.ts
+++ 04-store-api/starter/controllers/products.ts
@@ -2,16 +2,45 @@ import Product from "../models/product";
 
 const getAllProductsStatic = async (req, res) => {
   // throw new Error("testing acync errors");
-  const products = await Product.find({ page: "2" });
+  const search = "ab";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  }).sort("-name price");
   res.status(200).json({ products, nbHits: products.length });
 };
+
+interface Query {
+  featured?: boolean;
+  company?: string;
+  search?: string;
+  name?: object;
+  sort?: string;
+}
+
 const getAllProducts = async (req, res) => {
-  const { featured } = req.qeury;
-  const qeuryObject = { featured };
+  const { featured, company, sort, name } = req.query;
+  const queryObject: Query = {};
+  // const queryObject = {};
   if (featured) {
-    qeuryObject.featured = featured === "true" ? true : false;
+    queryObject.featured = featured === "true" ? true : false;
   }
-  const products = await Product.find(req.query);
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
