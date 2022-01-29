4a8bc2c3d768fb05f5e8284a32a7d05835450be4
UJ528491 <hyj0829@gmail.com>
2022-01-29 22:59:59 +0900

    sign jwt, check auth, verify jwt

--- 05-JWT-Basics/starter/controllers/main.ts
+++ 05-JWT-Basics/starter/controllers/main.ts
@@ -1,14 +1,33 @@
 import CustomAPIError from "../errors/custom-error";
 
+const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     throw new CustomAPIError("Please provide email and password", 400);
   }
-  res.send("Fake Login/Register/Signup Route");
+  const id = new Date().getDate;
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user db created", token });
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    throw new CustomAPIError("Not authorized to access this route ", 401);
+  }
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: "Hello User",
