a9da1f5d0df123ad2a1561a789713b564e900efc
UJ528491 <hyj0829@gmail.com>
2022-02-04 23:13:59 +0900

    compare password for login

--- 06-jobs-api/starter/controllers/auth.ts
+++ 06-jobs-api/starter/controllers/auth.ts
@@ -1,6 +1,6 @@
 import { UserModel } from "../models/User";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError } from "../errors";
+import { BadRequestError, UnauthenticatedError } from "../errors";
 BadRequestError;
 
 const register = async (req, res) => {
@@ -10,7 +10,21 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Email and password are required");
+  }
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  // compare password with hash
+  const isPasswordValid = await user.comparePassword(password);
+  if (!isPasswordValid) {
+    throw new UnauthenticatedError("Invalid password");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 export { register, login };
