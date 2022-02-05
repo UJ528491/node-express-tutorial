e65b7d748ef31fe992dc7e1d15f49f36b6c433e7
UJ528491 <hyj0829@gmail.com>
2022-02-05 23:19:27 +0900

    getAllJobs function

--- 06-jobs-api/starter/controllers/jobs.ts
+++ 06-jobs-api/starter/controllers/jobs.ts
@@ -3,10 +3,12 @@ import { StatusCodes } from "http-status-codes";
 import { BadRequestError, NotFoundError } from "../errors";
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userID }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
-  res.send("get job");
+  const job = await Job.findById(req.params.id);
+  res.status(StatusCodes.OK).json({ job });
 };
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userID;
