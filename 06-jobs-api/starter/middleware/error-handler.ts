4b020bcb85221e9d7a65013e4c4a97daf73b35e3
UJ528491 <hyj0829@gmail.com>
2022-02-06 22:44:15 +0900

    duplicated error

--- 06-jobs-api/starter/middleware/error-handler.ts
+++ 06-jobs-api/starter/middleware/error-handler.ts
@@ -1,8 +1,21 @@
 import { StatusCodes } from "http-status-codes";
 import { CustomAPIError } from "../errors";
 export const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "somthing went wrong try again later",
+  };
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicated value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
