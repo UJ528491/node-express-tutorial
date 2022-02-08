4278c7d778e5f69f190843c8aba3e565286ccb4a
UJ528491 <hyj0829@gmail.com>
2022-02-08 23:23:40 +0900

    07-file-upload initial commit

--- 06-jobs-api/starter/models/User.ts
+++ 06-jobs-api/starter/models/User.ts
@@ -34,7 +34,7 @@ UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userID: this._id, name: this.name },
     process.env.JWT_SECRET,
-    { expiresIn: "30d" }
+    { expiresIn: process.env.JWT_LIFETIME }
   );
 };
 
