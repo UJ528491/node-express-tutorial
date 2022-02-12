9364ff57a93aba0c51e8dac682e110f12e46fe23
UJ528491 <hyj0829@gmail.com>
2022-02-12 23:46:27 +0900

    express-fileupload pack

--- 07-file-upload/starter/app.ts
+++ 07-file-upload/starter/app.ts
@@ -3,6 +3,7 @@ require("express-async-errors");
 
 const express = require("express");
 const app = express();
+import fileUpload from "express-fileupload";
 
 // database
 import connectDB from "./db/connect";
@@ -14,7 +15,9 @@ import { router } from "./routes/productRoutes";
 import notFoundMiddleware from "./middleware/not-found";
 import errorHandlerMiddleware from "./middleware/error-handler";
 
+app.use(express.static("./public"));
 app.use(express.json());
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("<h1>File Upload Starter</h1>");
--- 07-file-upload/starter/controllers/uploadsController.ts
+++ 07-file-upload/starter/controllers/uploadsController.ts
@@ -1,6 +1,15 @@
 import { StatusCodes } from "http-status-codes";
+import path from "path";
 
 const uploadProductImage = async (req, res) => {
-  res.send("upload product image");
+  const productImage = req.files.image;
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 export default uploadProductImage;
