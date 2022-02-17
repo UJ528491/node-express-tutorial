525278964bcbec7dce3eb3d9161d9571dc0c9d06
UJ528491 <hyj0829@gmail.com>
2022-02-18 03:04:54 +0900

    Ethereal email and sendGrid email

--- 08-send-email/starter/controllers/sendEmail.ts
+++ 08-send-email/starter/controllers/sendEmail.ts
@@ -1,5 +1,39 @@
-const sendEmail = (req, res) => {
-  res.send("<h1>Email Send</h1>");
+const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+
+const sendEmailEthereal = async (req, res) => {
+  let testAccount = nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "jeanne.hoeger26@ethereal.email",
+      pass: "4ZmStD2zxBEXMXsyHt",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"UJ 👻" <hyj0829@gmail.com>', // sender address
+    to: "kalitsnya@naver.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  res.json(info);
+};
+
+const sendEmail = async (req, res) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "kalitsnya@naver.com", // Change to your recipient
+    from: "hyj0829@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  const info = await sgMail.send(msg);
+  res.json(info);
 };
 
 export default sendEmail;
