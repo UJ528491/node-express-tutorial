442e834f97d2cfe8c859c3bf17f816106f033fb8
UJ528491 <hyj0829@gmail.com>
2022-02-27 23:32:35 +0900

    register setup

--- 10-e-commerce-api/starter/controllers/authController.ts
+++ 10-e-commerce-api/starter/controllers/authController.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import User from "../models/User";
+import { StatusCodes } from "http-status-codes";
+import { CustomAPIError } from "../errors";
+
 const register = async (req: any, res: express.Response) => {
-  res.send("register user");
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json(user);
 };
 const login = async (req: any, res: express.Response) => {
   res.send("login user");
