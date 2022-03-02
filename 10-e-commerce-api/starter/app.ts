24d0d3941cf3ada53d7ca7e938d5d4f83dd9638a
UJ528491 <hyj0829@gmail.com>
2022-03-03 02:18:28 +0900

    signed and secure flags

--- 10-e-commerce-api/starter/app.ts
+++ 10-e-commerce-api/starter/app.ts
@@ -18,14 +18,15 @@ const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get("/", (req, res) => {
   res.send("e-commerce-api");
 });
 
 app.get("/api/v1", (req, res) => {
-  console.log(req.cookies);
+  // console.log(req.cookies);
+  console.log(req.signedCookies);
   res.send("e-commerce-api");
 });
 
