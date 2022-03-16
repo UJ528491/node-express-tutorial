83ba3ee14b4f62ce080553bbdda0030b587a082c
UJ528491 <hyj0829@gmail.com>
2022-03-16 16:44:43 +0900

    review model

---
+++ 10-e-commerce-api/starter/models/Review.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please provide a rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a title"],
+      maxlength: [50, "Title cannot be more than 50 characters"],
+    },
+    comment: {
+      type: String,
+      required: [true, "Please provide a title"],
+      maxlength: [50, "Title cannot be more than 50 characters"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+export default mongoose.model("Review", ReviewSchema);
