086ee2630f4953b1f7aec2d2fefc5b423391e1de
UJ528491 <hyj0829@gmail.com>
2022-03-22 01:48:16 +0900

    aggregate pipline

--- 10-e-commerce-api/starter/models/Review.ts
+++ 10-e-commerce-api/starter/models/Review.ts
@@ -36,7 +36,34 @@ const ReviewSchema = new mongoose.Schema(
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 ReviewSchema.statics.calculateAverageRating = async function (productId) {
-  console.log(productId);
+  const result = await this.aggregate([
+    {
+      $match: {
+        product: productId,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numberOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 ReviewSchema.post("save", async function () {
