fdc11fb3bd8644865bdcfc4645f7403793b62103
UJ528491 <hyj0829@gmail.com>
2022-03-22 23:53:20 +0900

    order schema model

---
+++ 10-e-commerce-api/starter/models/order.ts
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const SingleCardItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  product: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [SingleCardItemSchema],
+    status: {
+      type: String,
+      enum: ["pending", "failed", "paid", "deliverd", "canceled"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntendId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
