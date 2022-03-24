ad68595742a838fbbefd9fa25f24546291f430ab
UJ528491 <hyj0829@gmail.com>
2022-03-24 23:59:16 +0900

    create order

--- 10-e-commerce-api/starter/controllers/orderController.ts
+++ 10-e-commerce-api/starter/controllers/orderController.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import { StatusCodes } from "http-status-codes";
+import CustomError from "../errors";
+import checkPermissions from "../utils/checkPermissions";
 
 const createOrder = async (req: any, res: express.Response) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("Cart is empty");
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError("Tax or Shipping Fee is missing");
+  }
   res.send("create order");
 };
 const getAllOrders = async (req: any, res: express.Response) => {
