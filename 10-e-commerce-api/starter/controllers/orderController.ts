454f7e584474a434412a43e85904db2ccb98ef10
UJ528491 <hyj0829@gmail.com>
2022-03-25 23:55:41 +0900

    check product in create order

--- 10-e-commerce-api/starter/controllers/orderController.ts
+++ 10-e-commerce-api/starter/controllers/orderController.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { StatusCodes } from "http-status-codes";
 import CustomError from "../errors";
+import Product from "../models/Product";
 import checkPermissions from "../utils/checkPermissions";
 
 const createOrder = async (req: any, res: express.Response) => {
@@ -11,6 +12,29 @@ const createOrder = async (req: any, res: express.Response) => {
   if (!tax || !shippingFee) {
     throw new CustomError.BadRequestError("Tax or Shipping Fee is missing");
   }
+
+  let orderItems: any[] = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(`Product ${item.product} not found`);
+    }
+    const { name, price, image, _id } = dbProduct;
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    // add item to orderItems
+    orderItems = [...orderItems, singleOrderItem];
+    // calculate subtotal
+    subtotal += item.amount * price;
+  }
+
   res.send("create order");
 };
 const getAllOrders = async (req: any, res: express.Response) => {
